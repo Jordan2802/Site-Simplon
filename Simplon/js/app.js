951b9ae1eb79d8b9eaae9a26a7cfb53fe2159222
Jordan2802 <sha08dow@hotmail.fr>
2018-08-01 12:30:48 +0200

    maj

--- Simplon/js/app.js
+++ Simplon/js/app.js
@@ -2,3 +2,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+     var elems = document.querySelectorAll('.fixed-action-btn');
+     var instances = M.FloatingActionButton.init(elems, {
+       direction: 'bottom',
+       hoverEnabled: false
+     });
+   });
