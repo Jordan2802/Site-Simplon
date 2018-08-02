84056af76d12c5b22c8a37cf6f5700d9081d69ba
Jordan2802 <sha08dow@hotmail.fr>
2018-08-02 13:02:13 +0200

    maj front ok

--- Simplon/js/app.js
+++ Simplon/js/app.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
   $('.sidenav').sidenav();
   $('.slider').slider();
+  $('#trigger').click(function() {
+    $('#overlay').fadeIn(300);
+  });
+  $('#close').click(function() {
+    $('#overlay').fadeOut(300);
+  });
+  $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+      },
+    });
 });
 
 
